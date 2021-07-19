e0e5b9c5355b0bbb3656d44c7c579209bca0451b
aboodKahlous <86069394+aboodKahlous@users.noreply.github.com>
2021-07-19 13:12:40 +0300

    SASS first commet
    Sass

---
+++ javascript.js
@@ -0,0 +1,33 @@
+
+// search popup
+
+function togglepopup(){
+    document.getElementById("popup-1").classList.toggle("active"); 
+}
+
+// scroll to top 
+
+var scrollButtom = $("#scroolup");
+$(window).scroll(function() {
+
+    if ($(this).scrollTop() >= 300) {
+        scrollButtom.fadeIn(1000);
+    } else if ($(this).scrollTop() <= 300) {
+        scrollButtom.fadeOut(1000);
+    }
+})
+scrollButtom.click(function() {
+    $("html,body").animate({ scrollTop: 0 }, 200);
+})
+
+
+// change active link
+
+$(".pagination a").on('click' , function(){
+    $(this).addClass('active').siblings().removeClass('active');
+})
+
+
+
+// animation on scroll
+AOS.init();
